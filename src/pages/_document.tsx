c28525db77d7732f5c07f8955159137f4f3c9223
theHabesa1 <yaredabera215@gmail.com>
2023-04-06 13:50:01 +0300

    document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,26 +36,7 @@ class IQDocument extends Document
                     <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
                     <link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' rel='stylesheet' />
 
-                    <script
-                        id='schema-structured-data'
-                        type="application/ld+json"
-                        dangerouslySetInnerHTML={{
-                            __html: JSON.stringify({
-                                '@context': 'https://schema.org/',
-                                '@type': 'Person',
-                                name: 'Yared Abera',
-                                author:
-                                        {
-                                            '@type': 'Person',
-                                            name: 'Yared'
-                                        },
-                                email: 'yaredabera215@gmail.com',
-                                image: 'https://intelligentquantum.dev/images/IntelligentQuantum.png',
-                                jobTitle: 'Full-Stack Developer',
-                                url: 'https://intelligentquantum.dev'
-                            })
-                        }}
-                    />
+        
                 </Head>
                 <body>
                     <Main />
