4178d4d05b4e6705b05c8eda57d52810ef361754
theHabesa1 <yaredabera215@gmail.com>
2023-12-13 16:09:11 +0300

    Update backend technologies in home.json

--- src/components/layouts/footer/footer.component.tsx
+++ src/components/layouts/footer/footer.component.tsx
@@ -7,14 +7,14 @@ const Footer = () =>
     return (
         <footer className={styles.footer}>
             <div className={styles.footerBox}>
-                <span>© 2021 - { new Date().getFullYear() } Yared Ab incollabration with IntelligentQuantum</span>
+                <span>© 2021 Yared Ab </span>
                 <a
                     href='https://github.com/'
                     target='_blank'
                     rel='noreferrer'
                 >
                     <BsGithub />
-                    Git Licenced 
+                    Git  
                 </a>
             </div>
         </footer>
